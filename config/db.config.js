30438ff185ed98e40dd756b16d3fc770dff2a246
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2023-03-15 07:20:09 +0500

    first commit

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+// put database configuration Constants here
+module.exports = {
+  HOST: "localhost", // where the database is hosted
+  USER: "root", // username
+  PASSWORD: "123456", //above user's password
+  DB: "testdb", //database name
+  dialect: "postgres", // which database you're using
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+};
